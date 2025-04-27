b5872062d9ebed61ec5d8f86248b009a3000eb18
Tresinari <leotaguiar0@gmail.com>
2025-04-27 02:55:22 -0300

    Initial commit

---
+++ src/test/java/br/com/treinos/APITreinos/ApiTreinosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinos.APITreinos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTreinosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
